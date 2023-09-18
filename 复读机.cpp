9fca3760e0925a8ac108bcb45487044e4d4998d9
NIHAOAWA <142315379+NIHAOAWA@users.noreply.github.com>
2023-09-18 19:56:22 +0800

    Create 复读机.cpp

---
+++ 复读机.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int user
+int main(){
+  while(true){
+      cout<<"你好凑东西的项目，复读机来看看吧"<<endl;
+      cout<<"tips：你输入什么都会复读+！"<<endl;
+      cin>>user;
+      cout<<user<<"!"<<endl;
+  }
+//声明一下，这不是主要的！！！
+}
