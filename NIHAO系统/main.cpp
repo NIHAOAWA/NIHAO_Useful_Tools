0f8692bf48cde1fa092e10a232d9f8ebc6be72d2
NIHAOAWA <142315379+NIHAOAWA@users.noreply.github.com>
2023-09-21 20:31:48 +0800

    Create main.cpp

---
+++ NIHAO系统/main.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int a;
+int main(){
+//你好做这个，时间很晚了。
+//大多数功能明天做，今天打个空壳
+  cout<<"欢迎来到NIHAO系统！"<<endl;
+  cout<<"请问您要做什么 1.计算器 2.chatbot"<<endl
+  cin>>a;
+  if(a==1){
+    //这是明天实现的功能！！！
+  }
+}  
